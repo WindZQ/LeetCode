66286a1d3d247cb42585fb2d9d4c87b5953721e2
WindZQ <1049498972@qq.com>
2023-06-03 12:20:56 +0800

    The Add Digits Problem Solution

---
+++ addDigits/addDigits.cpp
@@ -0,0 +1,33 @@
+#define method1 1
+#define method2 0
+
+
+class Solution
+{
+public:
+#if method1
+    int addDigits(int num)
+    {
+        while (num >= 10)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            num = sum;
+        }
+
+        return num;
+    }
+#endif
+
+#if method2
+    int addDigits(int num)
+    {
+        return (num - 1) % 9 + 1;
+    }
+#endif
+}
